3c8694cb47b84bf0deee0367c3c260a18185cb69
marslan-786 <marslansalfias@gmail.com>
2025-08-22 17:00:48 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ OWNER_USERNAME = "whatsapp_offcial"  # Replace with your actual Telegram Usernam
 
 API_ID = 94575
 API_HASH = 'a3406de8d171bb422bb6ddf3bbd800e2'
-BOT_TOKEN = '8324191756:AAF28XJJ9wSO2jZ5iFIqlrdEbjqHFX190Pk'
+BOT_TOKEN = '7984874762:AAGc99zaI2M6CC0hFWIftxQ6B6ZknsjfKKw'
 
 SESSION_FOLDER = 'sessions'
 CHANNEL_DATA_FILE = 'channel_data.json'
